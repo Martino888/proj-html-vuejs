8ec5832e2eb32e0afaa0c0f22bc4daabb1dd7254
Martino888 <maxmartino33@gmail.com>
2022-04-04 18:01:36 +0200

    inserimento sezione E

--- src/components/MainSezE.vue
+++ src/components/MainSezE.vue
@@ -7,9 +7,21 @@
             <div>
                 <button class="btn">Exsplore All Resources</button>
             </div>
-            <div></div>
         </div>
-
+        <div class="container d-flex justify-content-between">
+                <div>
+                    <img src="@/assets/images/blog-posts-6.jpg" alt="">
+                    <h4>Marketing Tips And Tricks For Your Website</h4>
+                </div>
+                <div>
+                    <img src="@/assets/images/blog-posts-5.jpg" alt="">
+                    <h4>How to Write Stunning Blog Post Titles</h4>
+                </div>
+                <div>
+                    <img src="@/assets/images/blog-posts-4.jpg" alt="">
+                    <h4>Techniques to Reduce Facebook Ads Spend</h4>
+                </div>
+        </div>
     </div>
 </template>
 
@@ -32,9 +44,17 @@ export default {
 .btn{
     background-color:  #f26f28;
     color:white;
-    padding: 5px;
     padding-left: 25px;
     padding-right: 25px;
+    margin: 210px 0px 0px 300px;
+}
+
+img {
+    width: 350px;
+}
+
+h5{
+    width: 300px;
 }
 
 
